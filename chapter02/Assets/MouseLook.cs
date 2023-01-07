7102c771b5dcd86807ba812668c694d1abc05ec2
Dmytro Honchar <dmytro.honchar972@gmail.com>
2023-01-07 18:05:25 +0200

    chapter two added

---
+++ chapter02/Assets/MouseLook.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Control Script/Mouse Look")]
+public class MouseLook : MonoBehaviour
+{
+    public enum RotationAxes
+    {
+        MouseXAndY = 0, 
+        MouseX = 1,
+        MouseY = 2
+    }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+
+    public float sensativityHor = 9.0f;
+    public float sensativityVert = 9.0f;
+
+    public float minimumVert = -45.0f;
+    public float maximumVert = 45.0f;
+
+    private float _rotationX = 0;
+
+    void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        if(body != null )
+        {
+            body.freezeRotation = true;
+        }
+    }
+
+    void Update()
+    {
+        if(axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensativityHor, 0);
+        }
+        else if(axes == RotationAxes.MouseY)
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * sensativityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+
+            //float rotationY = transform.localEulerAngles.y;
+
+            //transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+            transform.localEulerAngles = new Vector3(_rotationX, transform.localEulerAngles.y, 0);
+        }
+        else
+        {
+            float rotationY = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensativityHor;
+
+            _rotationX -= Input.GetAxis("Mouse Y") * sensativityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+
+            //float delta = Input.GetAxis("Mouse X") * sensativityHor;
+            //float rotationY = transform.localEulerAngles.y + delta;
+
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+    }
+}
